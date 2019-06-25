99dcb66d77594b21b3d0a84ba2590c1ce3d58ec0
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-25 08:47:22 -0300

    initializing backend

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+//middleware
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`Backend online on port ${port}`)
+})
