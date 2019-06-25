0b0a018a0fcd793f2592eff0e1c050ae002ebbcc
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-25 19:57:55 -0300

    criando schemas com mongoose

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,25 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const creditSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, min: 0, required: true },
+})
+
+const debtSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, min: 0, required: true },
+  status: { type: String, required: false, uppercase: true,
+            enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+})
+
+const billingCycleSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  month: { type: Number, min: 1, max: 12, required: true },
+  year: { type: Number, min: 1970, required: true },
+  day: { type: Number, min: 1, max: 31, required: false },
+  credits: [creditSchema],
+  debits: [debtSchema]
+})
+
+module.exports = restful.model('BillingCycle', billingCycleSchema)
