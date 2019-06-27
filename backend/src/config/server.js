09a913bcc30c7bf410b783a08ea497adb729eaac
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-27 09:09:51 -0300

    adding queryParser for skip and limit on get requests

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 //middleware
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`Backend online on port ${port}`)
