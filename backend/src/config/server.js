c9ddc45a7716a85701b52c8a80c519b6b76c8cbb
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-27 09:03:17 -0300

    enable cors on backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 //middleware
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`Backend online on port ${port}`)
