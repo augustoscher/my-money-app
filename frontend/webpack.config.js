44d569ef30400b72e87c6cdea0cb8c22d92c5d45
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-29 22:03:35 -0300

    setting up webpack 2

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,7 +17,35 @@ module.exports = {
     extensions: ['','.js','.jsx'],
     alias: {
       modules: __dirname + '/node_modules',
+      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
+  },
+  plugins: [
+    new webpack.ProviderPlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jquery': 'jquery'
+    }),
+    new ExtractTextPlugin('app.css')
+  ],
+  module: {
+    loaders: [{
+      test: /.js[x]?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-object-rest-spread']
+      }
+    }, 
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+    },
+    {
+      test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+      loader: 'file'
+    }]
   }
-
 }
