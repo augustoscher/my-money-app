9d0c4b7b47407dd125c5952bf4630729733ff178
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-01 09:08:29 -0300

    adding menuItem and menuTree component

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import MenuItem from './menuItem'
 
 export default props => (
   <ul className='sidebar-menu'>
-    
+    <MenuItem path='#' label='Dashboard' icon='dashboard' />
   </ul>
 )
