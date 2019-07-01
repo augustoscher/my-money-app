47ab41b58b45560f83b10e5cdab8a144d6071b88
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-30 23:05:56 -0300

    adding header component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+//middlewares
+import promise from 'redux-promise' //espera que a promise seja resolvida quando ela for retornada por um action creator
+import multi from 'redux-multi' //serve para retornar varias actions de um action creator
+import thunk from 'redux-thunk' //faz com que um action creator retorne um metodo
+
+import App from './main/app'
+import reducers from './main/reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('app')
+)
---
+++ frontend/src/main/testReducer.jsx
@@ -0,0 +1,6 @@
+const INITIAL_STATE = []
+
+export default (state = INITIAL_STATE, action) => {
+  console.log("test")
+  return state
+}
