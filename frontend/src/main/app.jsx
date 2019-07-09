d4987393cdbd0aaa00a118f699dffae318900e39
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-08 22:46:40 -0300

    addin routes, content and contentHeader components

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,15 +4,16 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
-  <div className='wrapper'>
-    <Header />
-    <SideBar />
-    <div className='content-wrapper'>
-      <h1>Conteudo</h1>
+    <div className='wrapper'>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'> 
+            {/* {props.children} */}
+            <Routes />
+        </div>
+        <Footer />
     </div>
-    <Footer />
-  </div>
 )
-
