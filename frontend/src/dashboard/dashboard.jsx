142de59c7515621d8e19c449a0c48917b9268505
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-09 00:31:37 -0300

    124 - row component

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
 import ValueBox from '../common/widget/valueBox';
+import Row from '../common/layout/row'
 
 class Dashboard extends React.Component {
   constructor(props){
@@ -13,12 +14,14 @@ class Dashboard extends React.Component {
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
         <Content>
-          <ValueBox cols='12 4' color='green' icon='bank'
-            value='R$ 10,00' text='Total de Créditos' />
-          <ValueBox cols='12 4' color='red' icon='credit-card'
-            value='R$ 10,00' text='Total de Débitos' />
-          <ValueBox cols='12 4' color='blue' icon='money'
-            value='R$ 0,00' text='Valor Consolidado' />
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank'
+              value='R$ 10,00' text='Total de Créditos' />
+            <ValueBox cols='12 4' color='red' icon='credit-card'
+              value='R$ 10,00' text='Total de Débitos' />
+            <ValueBox cols='12 4' color='blue' icon='money'
+              value='R$ 0,00' text='Valor Consolidado' />
+          </Row>
         </Content>
       </div>
     )
