9801ed32b4782aa8891929c5bcef620b06f0738c
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-08 22:57:04 -0300

    changes on dashboard component

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
-export default props => (
-  <h1>Dashboard</h1>
-)
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
+
+class Dashboard extends React.Component {
+  constructor(props){
+    super(props)
+  }  
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='Versão 1.0' />
+        <Content>
+          Dashboard
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
