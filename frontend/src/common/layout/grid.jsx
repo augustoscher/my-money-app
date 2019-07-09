47c0740b0ba08047e8cf27e4216a59c1d3032c86
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-09 00:11:44 -0300

    124 - ValueBox component

--- frontend/src/common/layout/grid.jsx
+++ frontend/src/common/layout/grid.jsx
@@ -9,13 +9,13 @@ export default class Grid extends React.Component {
       classes += `col-xs-${cols[0]}`
     }
     if (cols[1]) {
-      classes += `col-md-${cols[1]}`
+      classes += ` col-md-${cols[1]}`
     }
     if (cols[2]) {
-      classes += `col-sm-${cols[2]}`
+      classes += ` col-sm-${cols[2]}`
     }
     if (cols[3]) {
-      classes += `col-lg-${cols[3]}`
+      classes += ` col-lg-${cols[3]}`
     }
     return classes;
   }
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
+import ValueBox from '../common/widget/valueBox';
 
 class Dashboard extends React.Component {
   constructor(props){
@@ -12,7 +13,12 @@ class Dashboard extends React.Component {
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
         <Content>
-          Dashboard
+          <ValueBox cols='12 4' color='green' icon='bank'
+            value='R$ 10,00' text='Total de Créditos' />
+          <ValueBox cols='12 4' color='red' icon='credit-card'
+            value='R$ 10,00' text='Total de Débitos' />
+          <ValueBox cols='12 4' color='blue' icon='money'
+            value='R$ 0,00' text='Valor Consolidado' />
         </Content>
       </div>
     )
