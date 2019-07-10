583fdb3f32c4b60a893d781c1b50369d03281ef8
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-09 21:20:47 -0300

    132 - BillingCycle refactor

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-export default props => (
-  <div>
-    <h1>Billing Cycle</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class BillingCycle extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Billing Cycle' small='Cadastro'/>
+        <Content>
+          Billing Cycle
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default BillingCycle
