bd6b26e845f86d07942c9bfefc227e1e0307a087
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-08-05 22:15:15 -0300

    Input component for credit list

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -38,7 +38,7 @@ class BillingCycleForm extends React.Component {
             placeholder='Informe o ano'
             type='number'
             readOnly={readOnly} />
-          <CreditList cols='12 6' />
+          <CreditList cols='12 6' readOnly={readOnly} />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
