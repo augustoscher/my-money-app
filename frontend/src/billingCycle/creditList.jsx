21e1b2f511ae1f1cc8916c0db08c80b37762408b
Augusto Scher <augustoscher@gmail.com>
2019-08-06 22:54:13 -0300

    162 - add remove button to dynamic form

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Field, arrayInsert } from 'redux-form';
+import { Field, arrayInsert, arrayRemove } from 'redux-form';
 import Grid from '../common/layout/grid';
 import Input from '../common/form/input';
 
@@ -10,6 +10,12 @@ class CreditList extends React.Component {
   add(index, item = {}) {
     if (!this.props.readOnly) {
       this.props.arrayInsert('billingCycleForm', 'credits', index, item);
+    }
+  }
+
+  remove(index) {
+    if (!this.props.readOnly && this.props.list.length > 1) {
+      this.props.arrayRemove('billingCycleForm', 'credits', index);
     }
   }
 
@@ -40,6 +46,10 @@ class CreditList extends React.Component {
             onClick={() => this.add(index + 1, item)}>
             <i className='fa fa-clone'></i>
           </button>
+          <button type='button' className='btn btn-danger'
+            onClick={() => this.remove(index)}>
+            <i className='fa fa-trash-o'></i>
+          </button>
         </td>
 			</tr>
 		))
@@ -68,5 +78,5 @@ class CreditList extends React.Component {
 	}
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch)
 export default connect(null, mapDispatchToProps)(CreditList);
